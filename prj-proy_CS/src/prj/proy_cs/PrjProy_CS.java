abe98f1b887bbd7dc66ef89ed3cdbc82409b4572
Sebastian Villalobos <dereklevilla45@gmail.com>
2022-09-26 08:29:11 -0600

    ini Proyecto

---
+++ prj-proy_CS/src/prj/proy_cs/PrjProy_CS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prj.proy_cs;
+
+/**
+ *
+ * @author Derek
+ */
+public class PrjProy_CS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
