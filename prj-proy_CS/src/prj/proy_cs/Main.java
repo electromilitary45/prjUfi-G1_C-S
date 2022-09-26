2f55005b06bbbdfc01d5a7cb641b8ace01ad7d44
Sebastian Villalobos <dereklevilla45@gmail.com>
2022-09-26 17:55:44 -0600

    Update Main.java

--- prj-proy_CS/src/prj/proy_cs/Main.java
+++ prj-proy_CS/src/prj/proy_cs/Main.java
@@ -14,7 +14,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("prj.proy_cs.Main.main()");
     }
     
 }
